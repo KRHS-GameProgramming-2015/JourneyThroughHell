a9a9e4c1d3f2125b65531dc0dab013dce5888713
mfk100 <mason.kucharski@kearsarge.org>
2015-12-10 12:13:25 -0500

    made bosses and started player code

--- Player.py
+++ Player.py
@@ -1,1 +1,25 @@
+pygame.init()
 
+bgImage = pygame.image.load("Player1.png")
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: 
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                player.go("up")
+            elif event.key == pygame.K_DOWN:
+                player.go("down")
+            elif event.key == pygame.K_LEFT:
+                player.go("left")
+            elif event.key == pygame.K_RIGHT:
+                player.go("right")
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP:
+                player.go("stop up")
+            elif event.key == pygame.K_DOWN:
+                player.go("stop down")
+            elif event.key == pygame.K_LEFT:
+                player.go("stop left")
+            elif event.key == pygame.K_RIGHT:
+                player.go("stop right")
