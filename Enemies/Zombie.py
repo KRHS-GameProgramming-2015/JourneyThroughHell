028be5e7ad0a7da8e555c589748bc7cefd487359
OHeraDragonFire <Hunter.LaBelle@kearsarge.org>
2015-12-22 12:35:32 -0500

    zombie stuff

--- Enemies/Zombie.py
+++ Enemies/Zombie.py
@@ -1,6 +1,14 @@
 import sys, pygame, math, random
 pygame.init()
 
+class Zombie():
+    def __init__(self, weapon, pos = [0,0]):
+        self.speedx = 0
+        self.speedy = 0
+        self.speed = [self.speedx, self.speedy]
+        self.maxSpeedx = 5
+        self.maxSpeedy = 5
+
 ballTimer += 1
     if ballTimer >= ballTimerMax:
         ballTimer = 0
